3d7b432d4edaf354485adf064c237b5d425d5c5a
Moiz Ansari <moizansari.india@gmail.com>
2025-04-21 13:14:57 +0530

    Main

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
-import express from 'express';
-import { db } from './config/databse.js';
-import { userModel } from './models/userModel.js';
-import dotenv from 'dotenv'
-
+import express from "express";
+import { db } from "./config/databse.js";
+import { userModel } from "./models/userModel.js";
+import dotenv from "dotenv";
 
 const app = express();
-dotenv.config()
-app.use(express.json())
+dotenv.config();
+app.use(express.json());
 const Port = process.env.PORT || 3000;
 
-app.post('/signup', async (req, res) => {
-    try {
-        const user = new userModel({
-            firstName: 'aman',
-            lastName: 'Ansari',
-            email: 'an@gmail.com',
-            password: '123445'
-        });
-    
-        await user.save();
-    
-        res.send('User Successfully registred')
-        
-    } catch (error) {
-        res.send('User already Exist!!')   
-    }
-})
+app.post("/signup", async (req, res) => {
+  try {
+    console.log(req.body);
 
+    const user = new userModel(req.body);
 
+    await user.save();
 
+    res.send("User Successfully registred");
+  } catch (error) {
+    res.send("User already Exist!!"); 
+  }
+});
 
 app.listen(Port, async () => {
-    await db()
-    console.log(`Listening on ${Port}`);
-})
+  await db();
+  console.log(`Listening on ${Port}`);
+});
