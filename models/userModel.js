3512404568b38cebb2a93fc9ee22a2e03c90ccc2
Moiz Ansari <moizansari.india@gmail.com>
2025-04-22 20:18:14 +0530

    userSchema

--- models/userModel.js
+++ models/userModel.js
@@ -17,6 +17,16 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   age: Number,
   gender: String,
+  bio: {
+    type: String,
+    default: "Hi, I'm the part of this worlds"
+  },
+  photoURL: {
+    type: String,
+  },
+  skills: {
+    type: [String]
+  }
 });
 
 const userModel = mongoose.model("User", userSchema);
