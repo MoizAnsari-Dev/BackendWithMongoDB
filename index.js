5ea6cf02f0425379302818d8cc97889b8b79a02d
Moiz Ansari <moizansari.india@gmail.com>
2025-04-22 13:49:30 +0530

    Main

--- index.js
+++ index.js
@@ -18,7 +18,55 @@ app.post("/signup", async (req, res) => {
 
     res.send("User Successfully registred");
   } catch (error) {
-    res.send("User already Exist!!"); 
+    res.send("User already Exist!!");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const UserId = req.body.email;
+    const findUser = await userModel.find({ email: UserId });
+    if (findUser.length === 0) {
+      res.send("User cant found");
+    } else {
+      res.send(findUser);
+    }
+  } catch (error) {
+    res.send("User Not Found");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await userModel.find({});
+    res.send(user);
+  } catch (error) {
+    res.send("SomeThing went worng");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const user = req.body._id;
+  try {
+    if (!(await userModel.findById({ _id: user }))) {
+      res.send("Invalid UserId Cheack the iD");
+    } else {
+      const findUser = await userModel.findByIdAndDelete(user);
+      res.send("User successfully deleted");
+    }
+  } catch (error) {
+    res.status(404).send("Something Went Wrong with your side");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const user = req.body._id;
+    const userData = req.body;
+    await userModel.findByIdAndUpdate(user, userData);
+    res.send("User Successfully Updated");
+  } catch (error) {
+    res.send("SomeThing went Worng");
   }
 });
 
