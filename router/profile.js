b4be9bee37b8e73a0e1c0a4b9e00d08da7b4e709
Moiz Ansari <moizansari.india@gmail.com>
2025-05-01 22:51:48 +0530

    Now user can update data

--- router/profile.js
+++ router/profile.js
@@ -19,7 +19,8 @@ profileRouter.patch("/profile/edit", authUser, async (req, res) => {
       throw new Error("Invalid Edit Request!!");
     }
     const loggedin = req.user;
-    Object.keys(req.body).forEach((key) => loggedin[key] === req.body[key]);
+    Object.keys(req.body).forEach((key) => loggedin[key] = req.body[key]);
+    console.log(loggedin);
     await loggedin.save()
     res.send(`${loggedin.firstName} , you are updated`)
   } catch (error) {
