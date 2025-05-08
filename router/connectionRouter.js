a3479285ac4c36c6eb9f019b080614f20a1298f3
Moiz Ansari <moizansari.india@gmail.com>
2025-05-08 20:53:52 +0530

    Router

--- router/connectionRouter.js
+++ router/connectionRouter.js
@@ -1,16 +1,48 @@
-import express from 'express'
-import { authUser } from '../middleware/auth.js';
-
+import express from "express";
+import { authUser } from "../middleware/auth.js";
+import { connectionRequestModel } from "../models/connectionRequest.js";
 
 const connectionRouter = express.Router();
 
-connectionRouter.post('/sendconnection', authUser, async (req, res) => {
-    const user = req.body;
-  console.log('Connection requested send');
-  res.send(user.firstName + ' Connection to the request')
-});
+connectionRouter.post(
+  "/request/send/:status/:toID",
+  authUser,
+  async (req, res) => {
+    try {
+      const fromUser = req.user._id;
+      const toUser = req.params.toID;
+      const status = req.params.status;
 
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({
+          message: "invalid status types",
+        });
+      }
 
-export {
-    connectionRouter
-}
+      const existingConnection = await connectionRequestModel.findOne({
+        //MongoDB OR syntex $OR
+        $or: [
+          { fromID, toID },
+          { fromID: toID, toID: fromID },
+        ],
+      });
+
+      const connectionDetail = new connectionRequestModel({
+        fromUser,
+        toUser,
+        status,
+      });
+
+      const dataConnection = await connectionDetail.save();
+      res.json({
+        message: "Connection Request send Successfully",
+        dataConnection,
+      });
+    } catch (error) {
+      res.status(400).send("Error: " + error.message);
+    }
+  }
+);
+
+export { connectionRouter };
